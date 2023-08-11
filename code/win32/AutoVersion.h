345103e0752f7c0901fa91ecfc62b5715ca9f952
Wood Family <woodfamily2487@hotmail.com>
2023-08-11 17:01:24 +0100

    "Year-23,Month-08,Day-11,BuildNum-01"

--- code/win32/AutoVersion.h
+++ code/win32/AutoVersion.h
@@ -22,7 +22,7 @@ This file is part of Jedi Academy.
 #define VERSION_MINOR_RELEASE		08
 #define VERSION_INTERNAL_BUILD		11
 
-#define VERSION_STRING				"Year-23,Month-08,Day-11,BuildNum-06"
-#define VERSION_STRING_DOTTED		"Year-23,Month-08,Day-11,BuildNum-06"
+#define VERSION_STRING				"Year-23,Month-08,Day-11,BuildNum-01"
+#define VERSION_STRING_DOTTED		"Year-23,Month-08,Day-11,BuildNum-01"
 
 #endif // __AUTO_VERSION_HEADER
